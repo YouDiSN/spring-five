d1a0395f89b87aa99cb1aab1f56d64bdbf4f4ae4
YouDiSN <839494773@qq.com>
2017-10-04 16:51:00 +0800

    filter

---
+++ src/main/java/com/demo/springfive/demo/core/filter/LoggingReactiveFilter.java
@@ -0,0 +1,8 @@
+package com.demo.springfive.demo.core.filter;
+
+/**
+ * Created by xiwang on 10/4/17.
+ * LoggingReactiveFilter
+ */
+public class LoggingReactiveFilter implements ReactiveFilter {
+}
